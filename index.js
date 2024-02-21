3a59ff368b0e4d866d7ac28b9c93dba2de3a8e1c
rozannouval <rozannouval@gmail.com>
2024-02-21 12:28:02 +0700

    instalasi

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    return (
-        <h1>test</h1>
-    )
-})
+
+});
+
+//import library CORS 
+const cors = require('cors')
+
+//use cors 
+app.use(cors())
+
+//import body parser
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log(`app running at http://localhost:${port}`);
+});
